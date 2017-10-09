0994f77ec0718f49e1c3e69c47ab203e51b10d3f
Lauren Cowen <lauren.cowen@gmail.com>
2017-10-08 19:40:14 -0500

    bug fix

--- liri.js
+++ liri.js
@@ -26,33 +26,39 @@ for (i = 3; i < process.argv.length; i++) {
 
 
 if (entry === "my-tweets") {
-	getTweets();
- 
+    getTweets();
+
 
 } else if (entry === "spotify-this-song") {
-	spotifyThisSong(searchTerm);
-    
+    spotifyThisSong(searchTerm);
+
 } else if (entry === "movie-this") {
-	movieThis(searchTerm);
-	
+    movieThis(searchTerm);
+
 
 } else if (entry === "do-what-it-says") {
-	fs.readFile('random.txt', 'utf8', function (err, data) {	
-  if (err) throw err;
-  var arrayOfStrings = data.split(",");
-  // entry = arrayOfStrings[0];
-  // searchTerm = arrayOfStrings[1];
-  if(arrayOfStrings[0]==="spotify-this-song") {
-  	spotifyThisSong(arrayOfStrings[1]);
-  }
-});
+    fs.readFile('random.txt', 'utf8', function(err, data) {
+        if (err) throw err;
+        var arrayOfStrings = data.split(",");
+        if (arrayOfStrings[0] === "spotify-this-song") {
+            spotifyThisSong(arrayOfStrings[1]);
+        } else if (arrayOfStrings[0] === "my-tweets") {
+            getTweets();
+        } else if (arrayOfStrings[0] === "movie-this") {
+            movieThis(arrayOfStrings[1]);
+        }
+        else {
+        	console.log("Invalid Entry");
+        }
+
+    });
 
 } else {
     console.log("invalid entry");
 }
 
 function getTweets() {
-	var params = { count: 20 };
+    var params = { count: 20 };
     client.get("statuses/user_timeline", params, function(error, data) {
         for (i = 0; i < data.length; i++) {
             console.log("Time: " + data[i].created_at);
@@ -63,7 +69,7 @@ function getTweets() {
 }
 
 function spotifyThisSong(song) {
-	if (song === "") {
+    if (song === "") {
         song = "The Sign";
         spotify.search({ type: 'track', query: song }, function(err, data) {
             if (err) {
@@ -94,24 +100,24 @@ function spotifyThisSong(song) {
 }
 
 function movieThis(movie) {
-	if(movie==="") {
-		movie = "Mr. Nobody";
-	}
+    if (movie === "") {
+        movie = "Mr. Nobody";
+    }
 
     request('http://www.omdbapi.com/?t=' + movie + ' &y=&plot=short&apikey=40e9cece', function(error, response, body) {
-       
-        if(!error&&response.statusCode === 200) {
-        	console.log("Movie Title: " + JSON.parse(body).Title);
-        	console.log("Year: " + JSON.parse(body).Year);
-        	console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-        	console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-        	console.log("Country: " + JSON.parse(body).Country);
-        	console.log("Language: " + JSON.parse(body).Language);
-        	console.log("Plot: " + JSON.parse(body).Plot);
-        	console.log("Actors: " + JSON.parse(body).Actors);
 
-        
+        if (!error && response.statusCode === 200) {
+            console.log("Movie Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+
+
         }
-     
+
     });
 }
